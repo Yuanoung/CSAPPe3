dd5ee5e29c6c4ffff8165bf3d283c6a008a933dc
郑元源 <CokHerBail@outlook.com>
2018-10-19 17:38:07 +0800

    modify

--- example-code/ecf/blockone.c
+++ example-code/ecf/blockone.c
@@ -2,20 +2,21 @@
 
 int main(int argc, char **argv)
 {
-/* $begin blockone */
+    /* $begin blockone */
     sigset_t mask, prev_mask;
 
     Sigemptyset(&mask);
-    Sigaddset(&mask, SIGINT); 
+    Sigaddset(&mask, SIGINT);
 
     /* Block SIGINT and save previous blocked set */
-    Sigprocmask(SIG_BLOCK, &mask, &prev_mask);  
-    
+    Sigprocmask(SIG_BLOCK, &mask, &prev_mask);
+
     /* ...code region that will not be interrupted by SIGINT */
-
+    Sleep(10);
+    printf("Done\n");
     /* Restore previous blocked set, unblocking SIGINT */
-    Sigprocmask(SIG_SETMASK, &prev_mask, NULL); 
-
-/* $end blockone */
+    Sigprocmask(SIG_SETMASK, &prev_mask, NULL);
+    Sleep(5);
+    /* $end blockone */
     exit(0);
 }
